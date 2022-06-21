c572ce1742afb9c64b678f0959f43b5919092432
ChengCK18 <chinkheng_98@hotmail.com>
2022-06-21 23:47:47 +0800

    Added Firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,15 @@ import 'package:parker/target_vehicle_notify_field.dart';
 import 'package:parker/personal_vehicle_stats_but.dart';
 import 'package:parker/target_vehicle_notify_but.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -41,6 +49,19 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String vecPlateNum = "None";
+
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+  /*
+  Future<void> addUser() {
+    // Call the user's CollectionReference to add a new user
+    return users
+        .add({
+          'full_name': 'John Doe', 
+        })
+        .then((value) => print("User Added"))
+        .catchError((error) => print("Failed to add user: $error"));
+  }*/
 
   @override
   Widget build(BuildContext context) {
